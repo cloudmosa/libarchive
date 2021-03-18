fafb4c6106c3f0a9bfb9e767bf8b8ffe31e53bba
Suyuan Chang <suyuan@cloudmosa.com>
2021-03-17 20:54:38 -0700

    Add iOS support.

--- config.h
+++ config.h
@@ -2,8 +2,10 @@
 
 #if defined(OS_LINUX)
 #include "config_linux.h"
-#elif defined(OS_MACOSX)
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
 #include "config_macosx.h"
+#elif defined(OS_MACOSX) && defined(OS_IOS)
+#include "config_ios.h"
 #else
 #error Not supported platform
 #endif
